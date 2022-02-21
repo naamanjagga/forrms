7bc5e7e51faa707d0907908ead88cbbc66c0eb21
naamanjagga <namanjagga@cedcommerce.com>
2022-02-21 14:37:48 +0530

    calculator

---
+++ src/calculator.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <title>calculator</title>
+</head>
+<body>
+    <form action="<?php echo $_SERVER["PHP_SELF"]?>" method="post" >
+        <lable>Number 1</lable>
+        <input type="text" name= "num1"><br>
+        <lable>Number 2</lable>
+        <input type="text" name= "num2"><br>
+
+        <?php 
+    if(isset($_POST["calc"])){
+        $x = $_POST["num1"];
+        $y = $_POST["num2"];
+        if($_POST["calc"] == '+'){
+            $result = $x + $y;
+        } elseif($_POST["calc"] == "-"){
+            $result = $x - $y;
+        } elseif($_POST["calc"] == '*'){
+            $result = $x * $y;
+        } else {
+            $result = $x / $y;
+        }
+    echo 'result <input style="margin-left: 28px;" type="text" value="'.$result.'" ><br>';
+ }
+?> 
+
+        <input style="margin-left: 70px;" type="submit" name= "calc" value= "+">
+        <input type="submit" name= "calc" value= "-">
+        <input type="submit" name= "calc" value= "*">
+        <input type="submit" name= "calc" value= "/">
+
+
+
+       
+
+    </form>
+    
+</body>
+</html>
